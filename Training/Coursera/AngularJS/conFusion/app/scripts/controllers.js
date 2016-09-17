b5175be52e4a6de75b268609b495be33e82b117c
Vibhor Joshi <vibhorjoshi05@gmail.com>
2016-09-17 15:52:07 +0530

    $http introduction

--- Training/Coursera/AngularJS/conFusion/app/scripts/controllers.js
+++ Training/Coursera/AngularJS/conFusion/app/scripts/controllers.js
@@ -8,7 +8,12 @@ angular.module('confusionApp')
             $scope.filtText = '';
             $scope.showDetails = false;
 
-            $scope.dishes= menuFactory.getDishes();
+            $scope.dishes= {};
+            menuFactory.getDishes()
+            .then(
+                function(response){
+                    $scope.dishes = response.data;
+                });
 
                         
             $scope.select = function(setTab) {
@@ -69,11 +74,16 @@ angular.module('confusionApp')
         }])
 
         .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
+            
+            $scope.dish = {};
+            menuFactory.getDish(parseInt($stateParams.id,10))
+            .then(
+                function(response){
+                    $scope.dish = response.data;
+                }
+                );
+            
 
-            var dish= menuFactory.getDish(parseInt($stateParams.id,10));
-            
-            $scope.dish = dish;
-            
         }])
 
         .controller('DishCommentController', ['$scope', function($scope) {
@@ -97,7 +107,13 @@ angular.module('confusionApp')
         .controller('IndexController', ['$scope', 'menuFactory', 'corporateFactory', function($scope, menuFactory, corporateFactory){
             
             $scope.promotion= menuFactory.getPromotion(0);
-            $scope.dish= menuFactory.getDish(0);
+            $scope.dish= {};
+            menuFactory.getDish(0)
+            .then(
+                function(response){
+                $scope.dish = response.data;
+                }
+            );
             $scope.exeChef= corporateFactory.getLeader(3);
         }])
 
