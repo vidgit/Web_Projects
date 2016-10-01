951fc3b8588090feabd90447fe656d1f1eafd082
Vibhor Joshi <vibhorjoshi05@gmail.com>
2016-10-02 00:44:02 +0530

    Changes

--- Training/Coursera/Hybrid Mobile Application Development/Ionic/conFusion/www/js/app.js
+++ Training/Coursera/Hybrid Mobile Application Development/Ionic/conFusion/www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('conFusion', ['ionic', 'conFusion.controllers'])
+angular.module('conFusion', ['ionic', 'conFusion.controllers', 'conFusion.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -36,7 +36,8 @@ angular.module('conFusion', ['ionic', 'conFusion.controllers'])
     url: '/home',
     views: {
       'mainContent': {
-        templateUrl: 'templates/home.html'
+        templateUrl: '/templates/home.html',
+        controller: 'IndexController'
       }
     }
   })
@@ -45,7 +46,7 @@ angular.module('conFusion', ['ionic', 'conFusion.controllers'])
       url: '/aboutus',
       views: {
         'mainContent': {
-          templateUrl: 'templates/aboutus.html'
+          templateUrl: '/templates/aboutus.html'
         }
       }
     })
@@ -54,7 +55,7 @@ angular.module('conFusion', ['ionic', 'conFusion.controllers'])
       url: '/contactus',
       views: {
         'mainContent': {
-          templateUrl: 'templates/contactus.html'
+          templateUrl: '/templates/contactus.html'
         }
       }
     })
@@ -63,7 +64,7 @@ angular.module('conFusion', ['ionic', 'conFusion.controllers'])
       url: '/menu',
       views: {
         'mainContent': {
-          templateUrl: 'templates/menu.html',
+          templateUrl: '/templates/menu.html',
           controller: 'PlaylistsCtrl'
         }
       }
@@ -73,7 +74,7 @@ angular.module('conFusion', ['ionic', 'conFusion.controllers'])
     url: '/menu/:id',
     views: {
       'mainContent': {
-        templateUrl: 'templates/dishdetails.html',
+        templateUrl: '/templates/dishdetails.html',
         controller: ''
       }
     }
